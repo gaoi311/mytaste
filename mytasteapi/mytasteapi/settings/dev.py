6e95cc922100fd26c0b5fd8776ccd95304cf98a4
gaoi311 <13956965930@163.com>
2020-05-02 20:46:28 +0800

    收集静态文件

--- mytasteapi/mytasteapi/settings/dev.py
+++ mytasteapi/mytasteapi/settings/dev.py
@@ -167,8 +167,10 @@ CORS_ALLOW_HEADERS = (
 # static file
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    # os.path.join(BASE_DIR, "static")
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "upload")
 MEDIA_URL = "/media/"
--- mytastefont/src/settings.js
+++ mytastefont/src/settings.js
@@ -1,3 +1,4 @@
 export default {
+    // 'HOST': 'http://api.mytaste.com:8000',
     'HOST': 'http://api.mytaste.top',
 }
