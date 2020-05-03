557e593b4f003a0c51242c9edbaef3aaef54183a
gaoi311 <13956965930@163.com>
2020-05-03 10:39:44 +0800

    调整uwsgi信息

--- mytasteapi/mytasteapi/settings/prod.py
+++ mytasteapi/mytasteapi/settings/prod.py
@@ -99,7 +99,7 @@ DATABASES = {
         'NAME': 'myTaste',
         'USER': 'root',
         'PASSWORD': 'gy',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
